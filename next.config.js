0f4f0a49d6b160b21358de0a65bc86635560f6af
Lukas <profiles@goodthings.dev>
2025-04-05 11:25:55 +0100

    Add CSP config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,52 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              `default-src 'self'`,
+              `script-src 'self' 'unsafe-inline' 'unsafe-eval'`,
+              `style-src 'self' 'unsafe-inline'`,
+              `img-src 'self' blob: data:`,
+              `font-src 'self' data:`,
+              `connect-src 'self'`,
+              `media-src 'self'`,
+              `frame-src 'self'`,
+            ].join('; ')
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=()'
+          }
+        ]
+      }
+    ];
+  },
   images: {
     domains: ["localhost"],
   },
